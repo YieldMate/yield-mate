1342ffadda2160cb3d54d006606d7e25878b125e
Seweryn Kras <sewerynkras@gmail.com>
2023-05-02 12:13:49 +0200

    setup connect kit

--- web-app/src/pages/_app.tsx
+++ web-app/src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Yield Mate",
+  })
+);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />;
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 };
 
 export default MyApp;
