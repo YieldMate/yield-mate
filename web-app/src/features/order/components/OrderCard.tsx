3887c1ca591cdb251026d6448305de0c6a4ba378
Seweryn Kras <sewerynkras@gmail.com>
2023-05-17 23:14:05 +0200

    fill steps based on order status

--- web-app/src/features/order/components/OrderCard.tsx
+++ web-app/src/features/order/components/OrderCard.tsx
@@ -7,9 +7,17 @@ type OrderCardProps = {
   order: Order;
 };
 
+const STATUS_TO_STEPS_FILLED: Record<Order["status"], number> = {
+  pending: 2,
+  completed: 4,
+  withdrawn: 5,
+};
+
 export default function OrderCard({ order }: OrderCardProps) {
   const { id, paymentToken, paymentAmount, targetToken, targetAmount, status } =
     order;
+
+  const stepsFilled = STATUS_TO_STEPS_FILLED[status];
 
   const paymentTokenConfig = SUPPORTED_TOKENS.find(
     (token) => token.symbol === paymentToken
@@ -22,19 +30,23 @@ export default function OrderCard({ order }: OrderCardProps) {
     <article className="card w-[900px] flex-row bg-base-100 p-8 shadow-xl">
       <ul className="steps steps-vertical w-[200px]">
         <li className="step-primary step">Order placed</li>
-        <li className="step-primary step">
+        <li className={`step ${stepsFilled >= 2 ? "step-primary" : ""}`}>
           {paymentToken} redirected to a yielding protocol
         </li>
-        <li className="step">{targetToken} purchased </li>
-        <li className="step">
+        <li className={`step ${stepsFilled >= 3 ? "step-primary" : ""}`}>
+          {targetToken} purchased{" "}
+        </li>
+        <li className={`step ${stepsFilled >= 4 ? "step-primary" : ""}`}>
           {targetToken} redirected to a yielding protocol
         </li>
-        <li className="step">{targetToken} withdrawn</li>
+        <li className={`step ${stepsFilled >= 5 ? "step-primary" : ""}`}>
+          {targetToken} withdrawn
+        </li>
       </ul>
       <div className="divider divider-horizontal h-full"></div>
 
-      <div className="flex flex-col justify-between">
-        <h2 className="pb-4 pl-6 text-2xl font-bold">Order ID: {id}</h2>
+      <div className="flex flex-col justify-start gap-6">
+        <h2 className="pl-6 text-2xl font-bold">Order ID: {id}</h2>
         <div className="flex h-[112px] flex-row items-center gap-4 text-gray-400">
           <div className="flex w-[260px] shrink-0 flex-col">
             <TokenPreview className="pb-0" token={paymentTokenConfig} />
@@ -52,11 +64,15 @@ export default function OrderCard({ order }: OrderCardProps) {
             </span>
           </div>
         </div>
-        <div className="flex w-full flex-row justify-end gap-4">
+        <div className="mt-auto flex w-full flex-row justify-end gap-4">
           <label htmlFor="learn-more-modal" className="btn-ghost btn">
             How it works?
           </label>
-          <button className="btn-primary btn">Withdraw</button>
+          <button className="btn-primary btn" disabled={status === "withdrawn"}>
+            {status === "withdrawn"
+              ? "Withdrawn"
+              : `Withdraw ${status === "pending" ? paymentToken : targetToken}`}
+          </button>
         </div>
       </div>
     </article>
