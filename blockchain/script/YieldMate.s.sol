ed1be48e6b553cb709827fcc3e15a6ba39470d2a
spaceh3ad <jan.kwiatkowski.web3@gmail.com>
2023-06-06 01:25:05 +0200

    updated script

--- blockchain/script/YieldMate.s.sol
+++ blockchain/script/YieldMate.s.sol
@@ -5,15 +5,20 @@ import "forge-std/Script.sol";
 import "../src/libs/order-manager/OrderManager.sol";
 import {Quoter} from "../src/libs/order-manager/price-engine/Quoter.sol";
 import {Swaper} from "../src/libs/order-manager/swaper/Swaper.sol";
-
+import {Automation} from "../src/libs/automation/Automation.sol";
 import {Modules} from "../src/libs/order-manager/lib/Objects.sol";
+import {Vault} from "../src/libs/vault/Vault.sol";
 
 contract MyScript is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_PROD");
         vm.startBroadcast(deployerPrivateKey);
 
+        Vault vault = new Vault();
+
         OrderManager manager = new OrderManager();
+
+        Automation automation = new Automation(1, address(manager));
 
         Quoter quoter = new Quoter(
             0x1F98431c8aD98523631AE4a59f267346ea31F984,
@@ -27,7 +32,8 @@ contract MyScript is Script {
 
         manager.setUpModule(Modules.SWAPER, address(swaper));
         manager.setUpModule(Modules.QUOTER, address(quoter));
-        manager.setUpModule(Modules.VAULT, address(0));
+        manager.setUpModule(Modules.VAULT, address(vault));
+        manager.setUpModule(Modules.KEEPER, address(automation));
 
         console.log("manager: %s", address(manager));
 
